39f22ade78534a195b6995274cfcf099a6064e85
Tom Stagg <tomstagg@gmail.com>
2019-02-28 23:10:41 +0000

    subclass fields testing

--- FactoryMethodPattern/Pizzas/NYStylePepperoniPizza.cs
+++ FactoryMethodPattern/Pizzas/NYStylePepperoniPizza.cs
@@ -2,6 +2,10 @@
 {
     internal class NYStylePepperoniPizza : Pizza
     {
+        public NYStylePepperoniPizza()
+        {
+            test = "123";
+        }
         protected override string Name => "NY Style Sauce and Pepperoni Pizza";
 
         protected override string Dough => "Thin Crust Dough";
